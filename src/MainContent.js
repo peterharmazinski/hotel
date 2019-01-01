74f77eeeaa6247b1cb33a7f7a7651de9a6050f42
Peter Harmazinski <pharmazinski@gmail.com>
2019-01-01 14:43:21 -0600

    replace boilerplate react code with top level page content

---
+++ src/MainContent.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const MainContent = () => {
+    return (
+        <main>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+            <section></section>
+        </main>
+    );
+}
+
+export default MainContent;
