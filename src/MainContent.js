b980959de5a415022a07ce36b0e255e9bc1a289e
Peter Harmazinski <pharmazinski@gmail.com>
2019-01-01 19:38:42 -0600

    implement basic structure of hero section

--- src/MainContent.js
+++ src/MainContent.js
@@ -3,7 +3,10 @@ import React from 'react';
 const MainContent = () => {
     return (
         <main>
-            <section></section>
+            <section className="hero">
+                <h1>Welcome to Deluxe</h1>
+                <h2>Hotels & Resorts</h2>
+            </section>
             <section></section>
             <section></section>
             <section></section>
