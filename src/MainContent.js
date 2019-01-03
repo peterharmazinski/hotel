a3c4493631c253a056bccef1a085b54590fc4cff
Peter Harmazinski <pharmazinski@gmail.com>
2019-01-02 18:43:47 -0600

    add sections to index page

--- src/MainContent.js
+++ src/MainContent.js
@@ -5,12 +5,15 @@ const MainContent = () => {
         <main>
             <section className="hero">
                 <h1>Welcome to Deluxe</h1>
-                <h2>Hotels & Resorts</h2>
+                <h2>Hotels &amp; Resorts</h2>
             </section>
-            <section></section>
-            <section></section>
-            <section></section>
-            <section></section>
+            <section className="booking"></section>
+            <section className="welcome"></section>
+            <section className="rooms"></section>
+            <section className="counters"></section>
+            <section className="testimonials"></section>
+            <section className="blog"></section>
+            <section className="instagram"></section>
         </main>
     );
 }
